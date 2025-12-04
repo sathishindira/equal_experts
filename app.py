9829e9edc1988504650a36b53d48d000f7ce5944
sathishindira <67998423+sathishindira@users.noreply.github.com>
2025-12-04 07:45:17 +0530

    Enhance API with docstrings and timeout
    Added docstrings for API endpoints and improved timeout handling.

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
+"""
+importing requried modules to collect and build the gist api server with uvicorn
+"""
 from fastapi import FastAPI, HTTPException
 import requests
 import uvicorn
 
 app = FastAPI()
 
+"Gists api root module for the welecome message"
 @app.get('/')
 def root():
     return {'message': 'Welcome to GitHub Gists API', 'usage': 'GET /{username}'}
 
+"Gists api health status monitoring"
 @app.get('/health')
 def health():
     return {'status': 'healthy'}
-
+    
+"Gists api collection for the provided username"
 @app.get('/{username}')
 def get_gists(username: str):
-    response = requests.get(f'https://api.github.com/users/{username}/gists')
+    response = requests.get(f'https://api.github.com/users/{username}/gists',timeout=5)
 
     if response.status_code == 404:
         raise HTTPException(status_code=404, detail='User not found')
@@ -32,3 +38,4 @@ def get_gists(username: str):
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8080)
+    
